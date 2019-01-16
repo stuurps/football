d95eb26cbe8b3d557b56868b1d12f420c41b50be
wildcamping <stuartbarker@Stuarts-MBP.connect>
2019-01-16 20:16:10 +0000

    one hot encoding

--- Documents/betting/R Code/tensorflow_model.R
+++ Documents/betting/R Code/tensorflow_model.R
@@ -1,6 +1,7 @@
 library(keras)
+library(data.table)
 
-data <- md_train
+data <- df
 data$Date <- NULL
 data$FTHG <- NULL
 data$FTAG <- NULL
@@ -14,36 +15,55 @@ y <- (data[, "FTR"])
 y <- as.factor(y)
 
 x <- subset(data, select = -c(FTR))
-x <- data.matrix(x)
+
+for(i in names(x)){
+  if(length(table(x[[i]])) == 1){
+    print(i)
+    x[[i]] <- NULL
+  }
+}
+
+dmy <- dummyVars(" ~ .", data = x)
+x1 <- data.frame(predict(dmy, newdata = x))
+
+x1 <- data.matrix(x1)
+
 # one hot encode classes / create DummyFeatures
 levels(y) = 1:length(y)
 y = to_categorical(as.integer(y)-1, num_classes = 3)
-x <- as.matrix(x)
+
 # create sequential model
 model = keras_model_sequential()
 
 # add layers, first layer needs input dimension
 model %>%
-  layer_dense(input_shape = ncol(x), units = 10, activation = "relu") %>%
-  layer_dense(units = 10, activation = "relu") %>%
+  layer_dense(input_shape = ncol(x1), units = 50, activation = "relu") %>%
+  layer_dropout(rate = 0.1) %>% 
+  layer_dense(units = 200, activation = "relu") %>%
+  layer_dropout(rate = 0.1) %>% 
+  layer_dense(units = 200, activation = "relu") %>%
+  layer_dropout(rate = 0.1) %>% 
+  layer_dense(units = 200, activation = "relu") %>%
   layer_dense(units = 3, activation = "softmax")
 
 # add a loss function and optimizer
 model %>%
   compile(
     loss = "categorical_crossentropy",
-    optimizer = "adagrad",
+    optimizer = "Adam",
     metrics = "accuracy"
   )
 
 # fit model with our training data set, training will be done for 200 times data set
 fit = model %>%
   fit(
-    x = x,
+    x = x1,
     y = y,
     shuffle = T,
     batch_size = 5,
-    validation_split = 0.3,
+    validation_split = 0.2,
     epochs = 200
   )
 plot(fit)
+
+model %>% evaluate(x, y,verbose = 0)
